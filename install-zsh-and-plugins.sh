a99cd2a799ce53a2330b6f79749bef24187eca56
Johnson <j3.soon@msa.hinet.net>
2022-06-06 16:35:36 +0800

    Add oh my zsh installation script

---
+++ install-zsh-and-plugins.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+if [ -f $HOME/.zshrc ]; then
+    echo "Error: Already installed."
+    exit 1
+fi
+
+sudo apt-get install -y zsh
+echo "Installed $(zsh --version)."
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+echo "Installed oh-my-zsh."
+
+sudo apt-get install -y fzf
+echo "Installed fzf."
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "Installed zsh-autosuggestions."
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Installed zsh-syntax-highlighting."
+
+PLUGINS=$(cat << EOF
+plugins=(
+    git
+    zsh-autosuggestions
+    zsh-interactive-cd # sudo apt-get install fzf
+    zsh-syntax-highlighting
+)
+
+EOF
+)
+PLUGINS=$(echo "$PLUGINS" | sed -z 's/\n/\\n/g')
+sed -i "s/^plugins=.*$/$PLUGINS/g" ~/.zshrc
+echo "Installed plugins."
+
+echo "Done."
+
+echo "[Optional] Changing default shell... (Ctrl+C to cancel)"
+chsh -s $(which zsh)
+echo "Changed default shell."
+
+exec zsh
